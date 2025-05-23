f2e8fad8ca651465d6a91b2830e761b67fad41bd
Oleg Nemanov <lego12239@yandex.ru>
2025-05-23 23:42:05 +0300

    bump version to 1.0

--- error_box.js
+++ error_box.js
@@ -1,4 +1,4 @@
-/* error_box | error_box 0.1.0 | License - GNU LGPL 3 */
+/* error_box | error_box 1.0 | License - GNU LGPL 3 */
 /*
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
