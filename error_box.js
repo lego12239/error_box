17b7ee79fbe87184a3d97e45092e3d77b1329c0d
Oleg Nemanov <lego12239@yandex.ru>
2025-05-23 23:48:29 +0300

    fix license clause

--- error_box.js
+++ error_box.js
@@ -1,4 +1,4 @@
-/* error_box | error_box 1.0 | License - GNU LGPL 3 */
+/* error_box | error_box 1.0 | License - BSD 2-clause */
 /*
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
